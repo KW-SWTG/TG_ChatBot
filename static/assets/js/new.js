9a2ff6b64d4fd18b42636b0b35a2587ef4cc3b21
juyeon <white6689@naver.com>
2020-11-24 00:50:14 +0900

    스타일 적용 3

--- static/assets/js/new.js
+++ static/assets/js/new.js
@@ -24,6 +24,80 @@ function newEntry() {
   }
 }
 
+function btn_click()
+{
+    count=count+1;
+
+    lastUserMessage = document.getElementById("chatbox").value;
+
+    var for_user_one=document.createElement("div");
+    for_user_one.setAttribute("class", "outgoing_msg");
+    for_user_one.setAttribute("id", "outgoing_msg"+count);
+    parent_id=document.getElementById("msg_list_bx_real");//여기에다가 메세지를 만들어 줄거다! parent_id.appendChild("d")l해서
+    parent_id.appendChild(for_user_one);
+  //create("outgoing_msg","outgoing_msg"+count, parent_id);
+
+    var for_user=document.createElement("div");
+    for_user.setAttribute("class", "sent_msg");
+    for_user.setAttribute("id", "sent_msg");
+    parent_id=document.getElementById("outgoing_msg"+count);//여기에다가 메세지를 만들어 줄거다! parent_id.appendChild("d")l해서
+    parent_id.appendChild(for_user);
+    //create("sent_msg","sent_msg", parent_id);
+    var text=document.createTextNode(lastUserMessage);
+    var p_1=document.createElement('p');
+    for_user.appendChild(p_1);
+    p_1.appendChild(text);
+
+    let date=new Date();
+    var date_txt=date.toLocaleTimeString();
+    alert(date.toLocaleTimeString());
+    var date_time=document.createElement("span");
+    date_time.setAttribute("class", "time_date");
+    var text_date=document.createTextNode(date_txt);
+    for_user.appendChild(date_time);
+    date_time.appendChild(text_date);
+    //2.챗봇 메세지 보이기
+
+    setTimeout(function() {
+    var for_chat_one=document.createElement("div");
+    for_chat_one.setAttribute("class", "incoming_msg");
+    for_chat_one.setAttribute("id", "incoming_msg"+count);
+    parent_id=document.getElementById("msg_list_bx_real");//여기에다가 메세지를 만들어 줄거다! parent_id.appendChild("d")l해서
+    parent_id.appendChild(for_chat_one);
+    //create("incoming_msg", "incoming_msg"+count,parent_id);
+    var for_chat=document.createElement("div");
+    for_chat.setAttribute("class", "received_msg");
+    for_chat.setAttribute("id", "received_msg"+count);
+    parent_id=document.getElementById("incoming_msg"+count);//여기에다가 메세지를 만들어 줄거다! parent_id.appendChild("d")l해서
+    parent_id.appendChild(for_chat);
+    //create("received_msg","received_msg"+count,parent_id);
+    var for_chat_last=document.createElement("div");
+    for_chat_last.setAttribute("class", "received_withd_msg");
+    for_chat_last.setAttribute("id", "received_withd_msg");
+    parent_id=document.getElementById("received_msg"+count);
+    parent_id.appendChild(for_chat_last);
+    //create("received_withd_msg","received_withd_msg", parent_id);
+    var text_chat=document.createTextNode(lastUserMessage);
+    var p_2=document.createElement('p');
+    for_chat_last.appendChild(p_2);
+    p_2.appendChild(text_chat);
+
+    let date=new Date();
+    var date_txt=date.toLocaleTimeString();
+    var date_time=document.createElement("span");
+    date_time.setAttribute("class", "time_date");
+    var text_date=document.createTextNode(date_txt);
+    for_chat_last.appendChild(date_time);
+    date_time.appendChild(text_date);
+    }, 1000);
+
+
+    document.getElementById("chatbox").value = "";
+
+
+}
+
+
 document.onkeypress = keyPress;
 //if the key pressed is 'enter' runs the function newEntry()
 function keyPress(e) {
@@ -37,21 +111,35 @@ function keyPress(e) {
     //outgoing_msg
     //1. 유저 메세지 보이기
     count=count+1;
+
     lastUserMessage = document.getElementById("chatbox").value;
+
     var for_user_one=document.createElement("div");
     for_user_one.setAttribute("class", "outgoing_msg");
     for_user_one.setAttribute("id", "outgoing_msg"+count);
     parent_id=document.getElementById("msg_list_bx_real");//여기에다가 메세지를 만들어 줄거다! parent_id.appendChild("d")l해서
     parent_id.appendChild(for_user_one);
+  //create("outgoing_msg","outgoing_msg"+count, parent_id);
+
     var for_user=document.createElement("div");
     for_user.setAttribute("class", "sent_msg");
     for_user.setAttribute("id", "sent_msg");
     parent_id=document.getElementById("outgoing_msg"+count);//여기에다가 메세지를 만들어 줄거다! parent_id.appendChild("d")l해서
     parent_id.appendChild(for_user);
+    //create("sent_msg","sent_msg", parent_id);
     var text=document.createTextNode(lastUserMessage);
     var p_1=document.createElement('p');
     for_user.appendChild(p_1);
     p_1.appendChild(text);
+
+    let date=new Date();
+    var date_txt=date.toLocaleTimeString();
+    alert(date.toLocaleTimeString());
+    var date_time=document.createElement("span");
+    date_time.setAttribute("class", "time_date");
+    var text_date=document.createTextNode(date_txt);
+    for_user.appendChild(date_time);
+    date_time.appendChild(text_date);
     //2.챗봇 메세지 보이기
 
     setTimeout(function() {
@@ -60,22 +148,51 @@ function keyPress(e) {
     for_chat_one.setAttribute("id", "incoming_msg"+count);
     parent_id=document.getElementById("msg_list_bx_real");//여기에다가 메세지를 만들어 줄거다! parent_id.appendChild("d")l해서
     parent_id.appendChild(for_chat_one);
+    //create("incoming_msg", "incoming_msg"+count,parent_id);
     var for_chat=document.createElement("div");
     for_chat.setAttribute("class", "received_msg");
-    for_chat.setAttribute("id", "received_msg");
+    for_chat.setAttribute("id", "received_msg"+count);
     parent_id=document.getElementById("incoming_msg"+count);//여기에다가 메세지를 만들어 줄거다! parent_id.appendChild("d")l해서
     parent_id.appendChild(for_chat);
+    //create("received_msg","received_msg"+count,parent_id);
+    var for_chat_last=document.createElement("div");
+    for_chat_last.setAttribute("class", "received_withd_msg");
+    for_chat_last.setAttribute("id", "received_withd_msg");
+    parent_id=document.getElementById("received_msg"+count);
+    parent_id.appendChild(for_chat_last);
+    //create("received_withd_msg","received_withd_msg", parent_id);
     var text_chat=document.createTextNode(lastUserMessage);
     var p_2=document.createElement('p');
-    for_chat.appendChild(p_2);
+    for_chat_last.appendChild(p_2);
     p_2.appendChild(text_chat);
+
+    let date=new Date();
+    var date_txt=date.toLocaleTimeString();
+    var date_time=document.createElement("span");
+    date_time.setAttribute("class", "time_date");
+    var text_date=document.createTextNode(date_txt);
+    for_chat_last.appendChild(date_time);
+    date_time.appendChild(text_date);
     }, 1000);
+
 
     document.getElementById("chatbox").value = "";
     //2. 챗봇 메세지 보이기
 
+    //3. 날짜 추가
+
   }
-}
+}//<span class="time_date">p다음에 ㅠㅠ
+//arguments[i]로 arguments들을 불러올수 있다.
+//arguments.length
 function placeHolder() {
   document.getElementById("chatbox").placeholder = "";
 }
+function create()
+{
+    var element=document.createElement("div");
+    element.setAttribute("class",arguments[0]);
+    element.setAttribute("id",arguments[1]);
+    arguments[2].appendChild(element);
+}
+
