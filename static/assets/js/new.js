444789756bb8e5ae36ea0be55983274e6f49ba79
JEEWOO SUL <39071676+jeewoo1025@users.noreply.github.com>
2020-11-26 19:25:06 +0900

    alert 제거 and 코드 정리 part1

--- static/assets/js/new.js
+++ static/assets/js/new.js
@@ -3,12 +3,9 @@ var parent_id="";
 var usr_msg="";
 var whyrano="";
 
-
 var messages = [], //array that hold the record of each string in chat
   lastUserMessage = "", //keeps track of the most recent input string from the user
   botName = 'Chatbot';//name of the chatbot
-
-
 
 function newEntry() {
   //if the message from the user isn't empty then run
@@ -91,12 +88,8 @@ function btn_click()
     date_time.appendChild(text_date);
     }, 1000);
 
-
     document.getElementById("chatbox").value = "";
-
-
 }
-
 
 document.onkeypress = keyPress;
 //if the key pressed is 'enter' runs the function newEntry()
@@ -112,6 +105,7 @@ function keyPress(e) {
     //1. 유저 메세지 보이기
     count=count+1;
 
+    // 입력 받는 거
     lastUserMessage = document.getElementById("chatbox").value;
 
     var for_user_one=document.createElement("div");
@@ -134,7 +128,7 @@ function keyPress(e) {
 
     let date=new Date();
     var date_txt=date.toLocaleTimeString();
-    alert(date.toLocaleTimeString());
+    //alert(date.toLocaleTimeString());
     var date_time=document.createElement("span");
     date_time.setAttribute("class", "time_date");
     var text_date=document.createTextNode(date_txt);
@@ -149,18 +143,21 @@ function keyPress(e) {
     parent_id=document.getElementById("msg_list_bx_real");//여기에다가 메세지를 만들어 줄거다! parent_id.appendChild("d")l해서
     parent_id.appendChild(for_chat_one);
     //create("incoming_msg", "incoming_msg"+count,parent_id);
+
     var for_chat=document.createElement("div");
     for_chat.setAttribute("class", "received_msg");
     for_chat.setAttribute("id", "received_msg"+count);
     parent_id=document.getElementById("incoming_msg"+count);//여기에다가 메세지를 만들어 줄거다! parent_id.appendChild("d")l해서
     parent_id.appendChild(for_chat);
     //create("received_msg","received_msg"+count,parent_id);
+
     var for_chat_last=document.createElement("div");
     for_chat_last.setAttribute("class", "received_withd_msg");
     for_chat_last.setAttribute("id", "received_withd_msg");
     parent_id=document.getElementById("received_msg"+count);
     parent_id.appendChild(for_chat_last);
     //create("received_withd_msg","received_withd_msg", parent_id);
+
     var text_chat=document.createTextNode(lastUserMessage);
     var p_2=document.createElement('p');
     for_chat_last.appendChild(p_2);
@@ -175,19 +172,19 @@ function keyPress(e) {
     date_time.appendChild(text_date);
     }, 1000);
 
-
     document.getElementById("chatbox").value = "";
     //2. 챗봇 메세지 보이기
 
     //3. 날짜 추가
-
   }
 }//<span class="time_date">p다음에 ㅠㅠ
 //arguments[i]로 arguments들을 불러올수 있다.
 //arguments.length
+
 function placeHolder() {
   document.getElementById("chatbox").placeholder = "";
 }
+
 function create()
 {
     var element=document.createElement("div");
